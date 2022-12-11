feb0b8b7d03034ecc1a34cf6154190b03f12bf50
Candela <candelaburgos10@yahoo.com>
2022-12-11 17:08:51 -0300

    Termine la logica js

---
+++ js/script.js
@@ -0,0 +1,37 @@
+const nums = document.querySelectorAll(".nums span");
+const counter = document.querySelector(".counter");
+const finalMessage = document.querySelector(".final");
+const replay = document.querySelector("#replay");
+
+const replayCountdown = () => {
+  counter.classList.remove("hide");
+  finalMessage.classList.remove("show");
+  nums.forEach((num) => {
+    num.classList.value = "";
+  });
+  nums[0].classList.add("in");
+};
+
+const playAnimation = () => {
+  nums.forEach((num, index) => {
+    const nextToLast = nums.length - 1;
+    num.addEventListener("animationend", (e) => {
+      if (e.animationName === "goIn" && index !== nextToLast) {
+        num.classList.remove("in");
+        num.classList.add("out");
+      } else if (e.animationName === "goOut" && num.nextElementSibling) {
+        num.nextElementSibling.classList.add("in");
+      } else {
+        counter.classList.add("hide");
+        finalMessage.classList.add("show");
+      }
+    });
+  });
+};
+
+playAnimation();
+
+replay.addEventListener("click", () => {
+  replayCountdown();
+  playAnimation();
+});
